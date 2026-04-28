b5f52199d1353d6f23dc090b2af59bf9d0bd20ec
Бексултан <nightwing_14@mail.ru>
2026-04-28 17:44:33 +0300

    Commit

---
+++ src/EmployeeBook.java
@@ -0,0 +1,110 @@
+import java.util.Arrays;
+
+public class EmployeeBook {
+    private Employee[] employees;
+
+    public EmployeeBook() {
+        this.employees = new Employee[10];
+    }
+
+    public double averageSalary() {
+        int salaries = 0;
+        int count = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                salaries = salaries + employee.getSalary();
+                count++;
+            }
+        }
+        return (double)salaries / count;
+    }
+
+    public void printEveryone() {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee);
+            }
+        }
+    }
+
+    public String toString() {
+       return Arrays.toString(employees);
+    }
+
+    public Boolean addEmployee(Employee employee) {
+        if (employee == null){
+            return false;
+        }for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null){
+                continue;
+            }else {
+                employees[i] = employee;
+                break;
+            }
+        }
+        return true;
+    }
+
+    public double taxes(Employee employee, String taxType) {
+        double tax = 0;
+        double salary = (double) employee.getSalary();
+        switch (taxType) {
+            case "PROPORTIONAL" :
+                tax = salary * 0.13;
+                break;
+            case "PROGRESSIVE":
+                if (employee.getSalary() <= 150) {
+                    tax = salary * 0.13;
+                } else if (salary <= 350) {
+                    tax = salary * 0.17;
+                } else {
+                tax = salary * 0.21;
+                }
+            }
+        return tax;
+        }
+
+    public void findFirstFromDepartment(int department) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getDepartment() == department) {
+                employees[i].printShortInfo();
+                break;
+            }
+        }
+    }
+
+    public Boolean findBySalary(Employee employee) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null && employees[i].equals(employee)) {
+                return true;
+            }
+        }return false;
+    }
+
+    public void findEmployeesWithSalaryLower(int wage, int employeeNumber) {
+        int found = 0;
+        int i = 0;
+        while (i < employees.length && found < employeeNumber) {
+            if (employees[i] != null && employees[i].getSalary() < wage) {
+                System.out.println(employees[i]);
+                found++;
+            }
+            i++;
+            if (found >= employeeNumber) {
+                break;
+            }
+        }
+        if (found == 0) {
+            System.out.println("нет таких");
+        }
+    }
+
+    public Employee findById(int id) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getId() == id) {
+                return employees[i];
+            }
+        }
+        return null;
+    }
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
 void main() {
+    EmployeeBook employeeBook = new EmployeeBook();
+    Employee bob = new Employee("bob", 12, 1222);
+    Employee lee = new Employee("Lee", 2, 12313123);
+    Employee notLee = new Employee("Lee", 2, 12313123);
+    employeeBook.addEmployee(bob);
+    System.out.println(employeeBook.toString());
+    employeeBook.addEmployee(lee);
+    employeeBook.addEmployee(notLee);
+    System.out.println(employeeBook);
+    employeeBook.printEveryone();
+    System.out.println(employeeBook.averageSalary());
+    employeeBook.findFirstFromDepartment(2);
+    System.out.println(employeeBook.findBySalary(bob));
 
 }
